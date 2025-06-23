a0c6589b51e4e9ab0c9e59c88bf314e2b91b54ab
CB230494 <161616911+CB230494@users.noreply.github.com>
2025-06-23 10:37:54 -0600

    Update db_ingresos.py

--- db_ingresos.py
+++ db_ingresos.py
@@ -14,3 +14,18 @@ def obtener_ingresos():
     response = supabase.table("ingresos").select("*").execute()
     return response.data
 
+def eliminar_ingreso(id):
+    response = supabase.table("ingresos").delete().eq("id", id).execute()
+    return response
+
+def actualizar_ingreso(id, fecha, concepto, monto, observacion=""):
+    data = {
+        "fecha": fecha,
+        "concepto": concepto,
+        "monto": monto,
+        "observacion": observacion
+    }
+    response = supabase.table("ingresos").update(data).eq("id", id).execute()
+    return response
+
+
