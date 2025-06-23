c4b739b18f8f656b1ab5bfa76ed3a306b544b2a8
CB230494 <161616911+CB230494@users.noreply.github.com>
2025-06-23 10:21:25 -0600

    Update db_ingresos.py

--- db_ingresos.py
+++ db_ingresos.py
@@ -1,1 +1,16 @@
+from supabase_client import supabase
+
+def insertar_ingreso(fecha, concepto, monto, observacion=""):
+    data = {
+        "fecha": fecha,
+        "concepto": concepto,
+        "monto": monto,
+        "observacion": observacion
+    }
+    response = supabase.table("ingresos").insert(data).execute()
+    return response
+
+def obtener_ingresos():
+    response = supabase.table("ingresos").select("*").execute()
+    return response.data
 
